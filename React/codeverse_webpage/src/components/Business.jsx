c137ebd44a217d85d976206338699b66217649b0
chamidu044 <145190960+chamidu044@users.noreply.github.com>
2024-03-10 01:51:07 +0530

    Chamidu - React Website
    React Webpage Styling

--- React/codeverse_webpage/src/components/Business.jsx
+++ React/codeverse_webpage/src/components/Business.jsx
@@ -3,6 +3,7 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 
+
 const FeatureCard = ({ icon, title, content, index }) => (
   <div className={`flex flex-row p-6 rounded-[20px] ${index !== features2.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
 
