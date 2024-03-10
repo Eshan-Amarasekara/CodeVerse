7ebfda17f7cd737ada43ffc87e0eec469623437f
chamidu044 <145190960+chamidu044@users.noreply.github.com>
2024-03-10 10:43:22 +0530

    Updated Hero.jsx
    Hero.jsx update

--- React/codeverse_webpage/src/components/Hero.jsx
+++ React/codeverse_webpage/src/components/Hero.jsx
@@ -36,14 +36,14 @@ const Hero = () => {
               onClick={scrollToWhatsNew}
             >
               <span className="flex items-center space-x-5">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-lime-400 -rotate-6 animate-spin"
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-gray-100 -rotate-6 animate-spin"
                   fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
                   <path strokeLinecap="round" strokeLinejoin="round"
                     d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
                 </svg>
-                <span className="pr-6 text-2xl text-gray-300">CodeVerse Release 2.0</span>
+                <span className="pr-6 text-2xl text-gray-400">CodeVerse Release 2.0</span>
               </span>
-              <span className="pl-6 text-indigo-400 group-hover:text-purple-500 transition duration-100">Upload Your Photo &rarr;</span>
+              <span className="pl-6 text-purple-500 group-hover:text-purple-700 transition duration-100">Upload Your Photo &rarr;</span>
             </button>
           </div>
         </div>
